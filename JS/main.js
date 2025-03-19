c977e9172a9aec2fa0505935a67055eca75bbe7e
zahra aulia putri <auliaputrizahra235@gmail.com>
2025-03-19 12:05:21 +0700

    [FIX] Commit without PORTOFOLIO

---
+++ JS/main.js
@@ -0,0 +1,15 @@
+// console.log("halo duniaaa");
+// document.write("saya senang sekali")
+
+let namaAja = "zahra"; //came1Case
+console.log(namaAja);
+
+
+
+console.log(typeof namaAja);
+
+
+var nama1;
+const nama2 = "zahra"
+console.long(nama1)
+console.long(nama2)
